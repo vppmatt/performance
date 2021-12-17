f733fde780c10a037689dc581f6896d7f99074f8
Matt Thornfield <matt@capabilitytm.com>
2021-12-17 13:56:25 +0000

    chapter 2

--- Chapter 02/PerformanceExample1/src/Main.java
+++ Chapter 02/PerformanceExample1/src/Main.java
@@ -1,8 +1,14 @@
-import java.util.Date;
-
 public class Main {
 
 	public static void main(String[] args) {
+		/*
+		//This code is to be uncommented towards the end of chapter 2
+		try {
+			Thread.sleep(20000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+*/
 		PrimeNumbers primeNumbers = new PrimeNumbers();
 		Integer max = Integer.parseInt(args[0]);
 		primeNumbers.generateNumbers(max);
