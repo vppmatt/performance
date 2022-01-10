7a94404be4a2147d394f96e5a7fdb2020534a2f9
Matt Thornfield <matt@capabilitytm.com>
2022-01-10 14:00:17 +0000

    chapter 9

---
+++ Chapter 09/GarbageCollection/src/Customer.java
@@ -0,0 +1,21 @@
+
+public class Customer  {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+	
+	public Customer(String name) {
+		this.name = name;
+	}
+
+	public String toString() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
